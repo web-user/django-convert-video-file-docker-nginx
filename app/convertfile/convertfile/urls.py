40bd6a67c2d21d91d408d6112fa1756635300e60
vladimir <vladimir.piv@computools.com>
2020-01-09 11:01:45 +0200

    urls media

--- app/convertfile/convertfile/urls.py
+++ app/convertfile/convertfile/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('', include('uploading.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
